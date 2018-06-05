93d501a972d5b37cda34a870c57da66a4e489c54
a.tsyganenko <a.tsyganenko@intive.com>
2018-06-05 17:21:00 +0200

    deprecated jee dependencies basic - done

--- books.xml.printer/src/main/java/module-info.java
+++ books.xml.printer/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module books.xml.printer {
     requires books.printer.api;
     requires books.storage.api;
     requires java.xml.bind;
+
+
     opens books.xml.printer to java.xml.bind;
 
     provides books.printer.api.BookPrinter with books.xml.printer.XmlBooksPrinter;
